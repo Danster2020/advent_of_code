082531961f5652773c108f317996ae9519f938de
Danster2020 <daniel.bleckert@me.com>
2025-12-02 15:37:34 +0100

    day2task2

---
+++ 2025/day2/task2.py
@@ -0,0 +1,120 @@
+#  /$$      /$$  /$$$$$$  /$$$$$$$  /$$   /$$ /$$$$$$ /$$   /$$  /$$$$$$        /$$   /$$  /$$$$$$  /$$$$$$$  /$$$$$$$  /$$$$$$ /$$$$$$$  /$$       /$$$$$$$$        /$$$$$$   /$$$$$$  /$$$$$$$  /$$$$$$$$                         
+# | $$  /$ | $$ /$$__  $$| $$__  $$| $$$ | $$|_  $$_/| $$$ | $$ /$$__  $$      | $$  | $$ /$$__  $$| $$__  $$| $$__  $$|_  $$_/| $$__  $$| $$      | $$_____/       /$$__  $$ /$$__  $$| $$__  $$| $$_____/                         
+# | $$ /$$$| $$| $$  \ $$| $$  \ $$| $$$$| $$  | $$  | $$$$| $$| $$  \__/      | $$  | $$| $$  \ $$| $$  \ $$| $$  \ $$  | $$  | $$  \ $$| $$      | $$            | $$  \__/| $$  \ $$| $$  \ $$| $$                               
+# | $$/$$ $$ $$| $$$$$$$$| $$$$$$$/| $$ $$ $$  | $$  | $$ $$ $$| $$ /$$$$      | $$$$$$$$| $$  | $$| $$$$$$$/| $$$$$$$/  | $$  | $$$$$$$ | $$      | $$$$$         | $$      | $$  | $$| $$  | $$| $$$$$                            
+# | $$$$_  $$$$| $$__  $$| $$__  $$| $$  $$$$  | $$  | $$  $$$$| $$|_  $$      | $$__  $$| $$  | $$| $$__  $$| $$__  $$  | $$  | $$__  $$| $$      | $$__/         | $$      | $$  | $$| $$  | $$| $$__/                            
+# | $$$/ \  $$$| $$  | $$| $$  \ $$| $$\  $$$  | $$  | $$\  $$$| $$  \ $$      | $$  | $$| $$  | $$| $$  \ $$| $$  \ $$  | $$  | $$  \ $$| $$      | $$            | $$    $$| $$  | $$| $$  | $$| $$                               
+# | $$/   \  $$| $$  | $$| $$  | $$| $$ \  $$ /$$$$$$| $$ \  $$|  $$$$$$/      | $$  | $$|  $$$$$$/| $$  | $$| $$  | $$ /$$$$$$| $$$$$$$/| $$$$$$$$| $$$$$$$$      |  $$$$$$/|  $$$$$$/| $$$$$$$/| $$$$$$$$                         
+# |__/     \__/|__/  |__/|__/  |__/|__/  \__/|______/|__/  \__/ \______/       |__/  |__/ \______/ |__/  |__/|__/  |__/|______/|_______/ |________/|________/       \______/  \______/ |_______/ |________/                         
+                                                                                                                                                                                                                                  
+                                                                                                                                                                                                                                  
+                                                                                                                                                                                                                                  
+#  /$$$$$$$  /$$$$$$$   /$$$$$$   /$$$$$$  /$$$$$$$$ /$$$$$$$$ /$$$$$$$         /$$$$$$  /$$$$$$$$       /$$     /$$ /$$$$$$  /$$   /$$ /$$$$$$$         /$$$$$$  /$$      /$$ /$$   /$$       /$$$$$$$  /$$$$$$  /$$$$$$  /$$   /$$
+# | $$__  $$| $$__  $$ /$$__  $$ /$$__  $$| $$_____/| $$_____/| $$__  $$       /$$__  $$|__  $$__/      |  $$   /$$//$$__  $$| $$  | $$| $$__  $$       /$$__  $$| $$  /$ | $$| $$$ | $$      | $$__  $$|_  $$_/ /$$__  $$| $$  /$$/
+# | $$  \ $$| $$  \ $$| $$  \ $$| $$  \__/| $$      | $$      | $$  \ $$      | $$  \ $$   | $$          \  $$ /$$/| $$  \ $$| $$  | $$| $$  \ $$      | $$  \ $$| $$ /$$$| $$| $$$$| $$      | $$  \ $$  | $$  | $$  \__/| $$ /$$/ 
+# | $$$$$$$/| $$$$$$$/| $$  | $$| $$      | $$$$$   | $$$$$   | $$  | $$      | $$$$$$$$   | $$           \  $$$$/ | $$  | $$| $$  | $$| $$$$$$$/      | $$  | $$| $$/$$ $$ $$| $$ $$ $$      | $$$$$$$/  | $$  |  $$$$$$ | $$$$$/  
+# | $$____/ | $$__  $$| $$  | $$| $$      | $$__/   | $$__/   | $$  | $$      | $$__  $$   | $$            \  $$/  | $$  | $$| $$  | $$| $$__  $$      | $$  | $$| $$$$_  $$$$| $$  $$$$      | $$__  $$  | $$   \____  $$| $$  $$  
+# | $$      | $$  \ $$| $$  | $$| $$    $$| $$      | $$      | $$  | $$      | $$  | $$   | $$             | $$   | $$  | $$| $$  | $$| $$  \ $$      | $$  | $$| $$$/ \  $$$| $$\  $$$      | $$  \ $$  | $$   /$$  \ $$| $$\  $$ 
+# | $$      | $$  | $$|  $$$$$$/|  $$$$$$/| $$$$$$$$| $$$$$$$$| $$$$$$$/      | $$  | $$   | $$             | $$   |  $$$$$$/|  $$$$$$/| $$  | $$      |  $$$$$$/| $$/   \  $$| $$ \  $$      | $$  | $$ /$$$$$$|  $$$$$$/| $$ \  $$
+# |__/      |__/  |__/ \______/  \______/ |________/|________/|_______/       |__/  |__/   |__/             |__/    \______/  \______/ |__/  |__/       \______/ |__/     \__/|__/  \__/      |__/  |__/|______/ \______/ |__/  \__/
+                                                                                                                                                                                                                                  
+                                                                                                                                                                                                                                  
+                                                                                                                                                                                                                                  
+
+
+puzzle_list = []
+with open("puzzle.txt", encoding="utf-8") as file:
+    for row in file:
+        puzzle_list.append(row.strip())
+
+interval_list = puzzle_list[0].split(",")
+numbers_list = []
+for interval in interval_list:
+    numbers_list.append(interval.split("-"))
+
+# print(numbers_list)
+
+def get_invalid_ids(start_stop_list):
+    invalid_ids = []
+    start_val = (start_stop_list[0])
+    stop_val = (start_stop_list[1])
+    
+    curr_val = start_val
+    for val in range(int(start_val), int(stop_val) +1):
+        curr_val = str(val)
+        curr_val_len = len(curr_val)
+        
+        # if uneven (1, 3, 5, 7, 9)
+        if curr_val_len % 2 != 0:
+            if curr_val_len == 3:
+                if curr_val == curr_val[0] * 3:
+                    # print("added len 3: ", curr_val, curr_val[0] * 3)
+                    invalid_ids.append(val)
+                    continue
+            if curr_val_len == 5:
+                if curr_val == curr_val[0] * 5:
+                    # print("added len 5: ", curr_val, curr_val[0] * 5)
+                    invalid_ids.append(val)
+                    continue
+            if curr_val_len == 7:
+                if curr_val == curr_val[0] * 7:
+                    # print("added len 7: ", curr_val, curr_val[0] * 7)
+                    invalid_ids.append(val)
+                    continue
+            elif curr_val_len == 9:
+                # print("test len 9: ", curr_val, curr_val[0:3] * 3)
+                if curr_val == curr_val[0:3] * 3:
+                    # print("added len 9 (3*3): ", curr_val, curr_val[0:3] * 3)
+                    invalid_ids.append(val)
+                    continue
+                if curr_val == curr_val[0] * 9:
+                    # print("added len 9: (1*9)", curr_val, curr_val[0] * 9)
+                    invalid_ids.append(val)
+                    continue
+                
+        if curr_val_len == 6:
+            if curr_val == curr_val[0:2] * 3:
+                # print("added len 6 (2*3): ", curr_val, curr_val[0:2] * 3)
+                invalid_ids.append(val)
+                continue
+            if curr_val == curr_val[0:3] * 2:
+                # print("added len 6 (3*2): ", curr_val, curr_val[0:3] * 2)
+                invalid_ids.append(val)
+                continue
+            
+        if curr_val_len == 8:
+            if curr_val == curr_val[0:2] * 4:
+                # print("added len 8 (2*4): ", curr_val, curr_val[0:2] * 4)
+                invalid_ids.append(val)
+                continue
+            if curr_val == curr_val[0:3] * 2:
+                # print("added len 8 (4*2): ", curr_val, curr_val[0:4] * 2)
+                invalid_ids.append(val)
+                continue
+            
+        if curr_val_len == 10:
+            if curr_val == curr_val[0:2] * 5:
+                # print("added len 10: ", curr_val, curr_val[0:2] * 5)
+                invalid_ids.append(val)
+                continue
+                
+        first_half, second_half = curr_val[:len(curr_val)//2], curr_val[len(curr_val)//2:]
+        if first_half == second_half:
+            # print("added", first_half, second_half)
+            invalid_ids.append(val)
+    
+    # print(start_val, stop_val)
+    return invalid_ids
+
+all_invalid_ids = []
+for element in numbers_list:
+    all_invalid_ids.append(get_invalid_ids(element))
+
+sum_of_all_invalid_ids = 0
+# all_invalid_ids = get_invalid_ids(numbers_list[1])
+for val_range in all_invalid_ids:
+    for value in val_range:
+        sum_of_all_invalid_ids += value
+
+# print(all_invalid_ids)
+print("sum:", sum_of_all_invalid_ids)
